0f409cedf556090015880c706bd87286e1f5b195
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2024-08-08 21:40:28 +0100

    v1.02

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react';
+import PostCreate from './postCreate'
+export default () => {
+    return( <div className="container">
+        <h1>Post App</h1>
+        <PostCreate />
     </div>
-  );
-}
-
-export default App;
+)}
