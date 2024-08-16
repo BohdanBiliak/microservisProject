9367e31b4b6a2aebf5d00f18aebacd21f3ae851c
BohdanBiliak <bohdan.biliak.detrox@gmail.com>
2024-08-16 20:29:18 +0100

    v1.04

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
 const app = express();
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(bodyParser.json())
@@ -17,13 +18,20 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {title} = req.body; 
 
     posts[id] = {
-        id, title
+        id, 
+        title
     }
+    await axios.post('http://localhost:4005/events', {
+        type: 'postCreated',
+        data:{
+            id, title
+        }
+    } )
 
 
     res.status(201).send(posts[id])
